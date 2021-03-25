763d287a2bd928a9ee886b522d0b5a51532ce59b
Gorkem Aksaray <gaksaray@ku.edu.tr>
2021-03-25 21:05:29 +0300

    Update code_manager.py
    **Issue**: When `png` is the display format and `graph_png_redundancy` is set to `False` (which is default), graph commands (such as `twoway`) produced `png` files at `.stata_kernel.cache` but the kernel could not display them within the notebook and was getting stuck at processing state.
    **Fix**: Repating the `gr export` when graph redundancy is set to false somehow fixes this problem.

--- stata_kernel/code_manager.py
+++ stata_kernel/code_manager.py
@@ -296,6 +296,7 @@ class CodeManager():
             g_exp = dedent("""
             if _rc == 0 {{
                 noi gr export `"{0}/graph${1}.{2}"',{3} replace
+                noi gr export `"{0}/graph${1}.{2}"',{3} replace
                 global {1} = ${1} + 1
             }}\
             """.format(cache_dir_str, gph_cnt, graph_fmt, dim_str))
